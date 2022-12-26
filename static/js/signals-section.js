769e1cc8d02282b7780fb376431d972606ded2df
Kamel Mohamed <112772763+Kamel-Mohammed@users.noreply.github.com>
2022-12-27 00:59:01 +0200

    chore: updating export/import - graphs

--- static/js/signals-section.js
+++ static/js/signals-section.js
@@ -9,45 +9,65 @@ var graph2Trace = {
   mode: "lines",
 };
 let cnt = 0;
-Plotly.newPlot("input-sig", [graph1Trace], get_signal_graph_layout());
-Plotly.newPlot("output-sig", [graph2Trace], get_signal_graph_layout());
+Plotly.newPlot(
+  "input-sig",
+  [graph1Trace],
+  get_signal_graph_layout("Input Signal", "", "Amplitude", [0, 100])
+);
+Plotly.newPlot(
+  "output-sig",
+  [graph2Trace],
+  get_signal_graph_layout("Output Signal", "", "Amplitude", [0, 100])
+);
 
 function draw_signals() {
   Plotly.extendTraces("input-sig", { y: [inputSignal] }, [0]);
   Plotly.extendTraces("output-sig", { y: [outputSignal] }, [0]);
 
   cnt++;
-  let xMove = {
-    xaxis: {
-      range: [cnt - 100, cnt],
-    },
-  };
 
   if (cnt > 100) {
-    Plotly.relayout("input-sig", xMove);
-    Plotly.relayout("output-sig", xMove);
+    Plotly.relayout(
+      "input-sig",
+      get_signal_graph_layout("Input Signal", "", "Amplitude", [cnt - 100, cnt])
+    );
+    Plotly.relayout(
+      "output-sig",
+      get_signal_graph_layout("Output Signal", "", "Amplitude", [
+        cnt - 100,
+        cnt,
+      ])
+    );
   }
 }
 
-function get_signal_graph_layout() {
+function get_signal_graph_layout(title, xTitle, yTitle, range) {
   return {
     width: 460,
     height: 180,
-    margin: { l: 45, r: 30, t: 30, b: 20 },
+    margin: { l: 55, r: 30, t: 45, b: 20 },
+    title: {
+      text: title,
+      font: {
+        family: "Roboto",
+        size: 20,
+      },
+    },
     xaxis: {
+      showticklabels: false,
       title: {
-        text: "time",
+        text: xTitle,
         font: {
           family: "Roboto",
           size: 14,
           color: "#000522e0",
         },
       },
-      range: [0, 100],
+      range: range,
     },
     yaxis: {
       title: {
-        text: "Amplitude",
+        text: yTitle,
         font: {
           family: "Roboto",
           size: 14,
